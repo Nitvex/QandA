19e82e32c290a025686e1d0e65680be2f328dc24
Nitvex <nikita_vozisov@epam.com>
2020-07-16 21:37:34 +0300

    [add] styles for homepage

--- frontend/src/Icons.tsx
+++ frontend/src/Icons.tsx
@@ -1,3 +1,15 @@
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import React from 'react';
 import user from './zondicons/user.svg';
-export const UserIcon = () => <img src={user} alt="User" width="12px" />;
+export const UserIcon = () => (
+  <img
+    css={css`
+      width: 12px;
+      opacity: 0.6;
+    `}
+    src={user}
+    alt="User"
+    width="12px"
+  />
+);
