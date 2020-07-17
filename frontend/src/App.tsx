c12dd025bb6103b28710e3389afff97fb0274363
Nitvex <nikita_vozisov@epam.com>
2020-07-17 16:55:56 +0300

    [add] components for Answer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,17 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { fontFamily, fontSize, gray2 } from './Styles';
 import './App.css';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import { AskPage } from './AskPage';
 import { SearchPage } from './SearchPage';
 import { SignInPage } from './SignInPage';
 import { QuestionPage } from './QuestionPage';
 import { NotFoundPage } from './NotFoundPage';
+
+const AskPage = lazy(() => import('./AskPage'));
 
 function App() {
   return (
@@ -27,7 +28,23 @@ function App() {
           <Redirect from="/home" to="/" />
           <Route exact path="/" component={HomePage} />
           <Route path="/search" component={SearchPage} />
-          <Route path="/ask" component={AskPage} />
+          <Route path="/ask" component={AskPage}>
+            <Suspense
+              fallback={
+                <div
+                  css={css`
+                    margin-top: 100px;
+                    text-align: center;
+                  `}
+                >
+                  {' '}
+                  Loading...
+                </div>
+              }
+            >
+              <AskPage />
+            </Suspense>
+          </Route>
           <Route path="/signin" component={SignInPage} />
           <Route path="/questions/:questionId" component={QuestionPage} />
           <Route component={NotFoundPage} />
