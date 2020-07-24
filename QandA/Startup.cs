5d136c30988c70309495fcea0108578029109b55
Nitvex <nikita_vozisov@epam.com>
2020-07-24 13:49:31 +0300

    [add] db creation and DataRepository

--- QandA/Startup.cs
+++ QandA/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using DbUp;
 
 namespace QandA
 {
@@ -26,6 +27,20 @@ namespace QandA
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges
+                .To
+                .SqlDatabase(connectionString, null)
+                .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+                .WithTransaction()
+                .Build();
+
+            if (upgrader.IsUpgradeRequired())
+            {
+                upgrader.PerformUpgrade();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +53,7 @@ namespace QandA
             else
             {
                 app.UseHttpsRedirection();
-            }          
+            }
 
             app.UseRouting();
 
